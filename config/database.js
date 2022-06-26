61fce7fa029cec44981c09b4ba9cd07ed99996d8
CAYDE <30632833+C-A-Y-D-E@users.noreply.github.com>
2022-06-27 00:11:07 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,13 +1,20 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'password'),
-      ssl: env.bool(true),
-    },
-  },
-});
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    
+    return {
+        connection: {
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: { rejectUnauthorized: false },
+            },
+            debug: false,
+        },
+    }
+};
